a37e69237a73f820c4d3fe6e84e95fc8f01b94ca
chiya <admin@chiya.ru>
2023-09-25 20:00:24 +0300

    Repository: sample data in dataclass

---
+++ data/src/main/java/ru/chiya/data/fakedb/CityTimezones.kt
@@ -0,0 +1,25 @@
+package ru.chiya.data.fakedb
+
+import ru.chiya.domain.R
+import ru.chiya.domain.model.Timezone
+
+val CityTimezones = listOf(
+    Timezone("Greenwich", R.drawable.twelve), // GMT+0
+    Timezone("Ürümqi", R.drawable.twelve),    // GMT+0
+    Timezone("City6", R.drawable.six),        // GMT+6
+    Timezone("City7", R.drawable.seven),      // GMT+7
+    Timezone("City8", R.drawable.eight),      // GMT+8
+    Timezone("City9", R.drawable.nine),       // GMT+9
+    Timezone("City10", R.drawable.ten),       // GMT+10
+    Timezone("City11", R.drawable.eleven),    // GMT+11
+    Timezone("City12", R.drawable.twelve),    // GMT+12
+    Timezone("City13", R.drawable.twelve), // GMT+0
+    Timezone("City14", R.drawable.twelve),    // GMT+0
+    Timezone("City15", R.drawable.six),        // GMT+6
+    Timezone("City16", R.drawable.seven),      // GMT+7
+    Timezone("City17", R.drawable.eight),      // GMT+8
+    Timezone("City18", R.drawable.nine),       // GMT+9
+    Timezone("City19", R.drawable.ten),       // GMT+10
+    Timezone("City20", R.drawable.eleven),    // GMT+11
+    Timezone("City21", R.drawable.twelve),    // GMT+12
+)
--- data/src/main/java/ru/chiya/data/repository/TimezonesRepositoryImpl.kt
+++ data/src/main/java/ru/chiya/data/repository/TimezonesRepositoryImpl.kt
@@ -1,11 +1,11 @@
 package ru.chiya.data.repository
 
+import ru.chiya.data.fakedb.CityTimezones
 import ru.chiya.domain.model.Timezone
 import ru.chiya.domain.repisotory.TimezonesRepository
 
 class TimezonesRepositoryImpl : TimezonesRepository {
     override fun getTimezones(amount: Int): List<Timezone> {
-        TODO("Not yet implemented")
+        return CityTimezones.shuffled().take(amount)
     }
-
 }
