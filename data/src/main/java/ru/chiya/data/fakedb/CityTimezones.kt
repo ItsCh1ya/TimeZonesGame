49cf826fc6cc71294e0ec455f0b393db49c84f5d
chiya <admin@chiya.ru>
2023-09-26 05:32:21 +0300

    DB: removed redundant cities

--- data/src/main/java/ru/chiya/data/fakedb/CityTimezones.kt
+++ data/src/main/java/ru/chiya/data/fakedb/CityTimezones.kt
@@ -4,8 +4,6 @@ import ru.chiya.domain.R
 import ru.chiya.domain.model.Timezone
 
 val CityTimezones = listOf(
-    Timezone("Greenwich", R.drawable.twelve), // GMT+0
-    Timezone("Ürümqi", R.drawable.twelve),    // GMT+0
     Timezone("City6", R.drawable.six),        // GMT+6
     Timezone("City7", R.drawable.seven),      // GMT+7
     Timezone("City8", R.drawable.eight),      // GMT+8
@@ -13,13 +11,4 @@ val CityTimezones = listOf(
     Timezone("City10", R.drawable.ten),       // GMT+10
     Timezone("City11", R.drawable.eleven),    // GMT+11
     Timezone("City12", R.drawable.twelve),    // GMT+12
-    Timezone("City13", R.drawable.twelve), // GMT+0
-    Timezone("City14", R.drawable.twelve),    // GMT+0
-    Timezone("City15", R.drawable.six),        // GMT+6
-    Timezone("City16", R.drawable.seven),      // GMT+7
-    Timezone("City17", R.drawable.eight),      // GMT+8
-    Timezone("City18", R.drawable.nine),       // GMT+9
-    Timezone("City19", R.drawable.ten),       // GMT+10
-    Timezone("City20", R.drawable.eleven),    // GMT+11
-    Timezone("City21", R.drawable.twelve),    // GMT+12
 )
